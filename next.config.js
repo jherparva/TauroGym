5fde44862222f137f37d602f2d9ac7cca3f068f2
jherparva <jhonvalderrama1990@gmail.com>
2025-05-07 22:55:07 -0500

    Descripción breve de los cambios

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
+    // Ignorar errores de ESLint durante la compilación
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Desactivar la verificación de tipos durante la compilación
+    // Ignorar errores de TypeScript durante la compilación
     ignoreBuildErrors: true,
   },
   images: {
@@ -22,24 +23,7 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === "production",
   },
   experimental: {
-    // Desactivar optimizaciones experimentales que pueden causar problemas
-    optimizeCss: false,
     scrollRestoration: true,
-  },
-  // Corregido: eliminar webpack5 y corregir la configuración de webpack
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": ".",
-    }
-
-    // Eliminar reglas de CSS
-    const oneOfRule = config.module.rules.find((rule) => typeof rule.oneOf === "object")
-    if (oneOfRule) {
-      oneOfRule.oneOf = oneOfRule.oneOf.filter((rule) => !rule.test || !rule.test.toString().includes("css"))
-    }
-
-    return config
   },
 }
 
